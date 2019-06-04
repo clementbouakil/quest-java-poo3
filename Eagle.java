51ae4420c6b4ac6397231f033f8f8eb9939883ec
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-06-04 17:37:34 +0200

    ajout des méthodes dans eagle

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,29 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println(this.name + " takes off " + " the sky")
+    }
+
+    @Override
+    public void ascend(int meters) {
+        System.out.println(this.name + " flies upward, altitude : " + this.altitude);
+    }
+
+    @Override
+    public void descend(int meters) {
+        System.out.println(this.name + " flies downward, altitude : " + this.altitude);
+    }
+
+    @Override
+    public void land() {
+        if this.altitude >= 10{
+            System.out.println(this.name + " is too high, it can't lands.");
+        } else {
+            System.out.println(this.name + " lands on the ground.");
+        }
     }
 }
